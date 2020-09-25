b607823c100fe284d0d10085cbf92296a836d8aa
Lygia Botelho <lygiasi@gmail.com>
2020-09-24 22:05:50 -0300

    adicionado arquivo netlify.toml

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import IframeComponent from './components/Iframe/Iframe';
+import Skybot from './components/Skybot';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <IframeComponent src="https://www.techo.org/brasil/" />
+      <Skybot/>
     </div>
   );
 }
